4eb715b0fe2b4896bf4f6576df29fb44e6a8488e
Neet Vaghasiya <vaghasiyaneet09@gmail.com>
2026-05-22 17:49:44 +0530

    nothing

--- python task/Day-4/task4.py
+++ python task/Day-4/task4.py
@@ -8,13 +8,13 @@ test_dtr_lst2 = test_dtr_lst.split()
 # print(test_dtr_lst2)
 # print(test_dtr_lst)
 
-test_dtr_str1 = test_dtr_lst1.pop(0)
-test_dtr_str1 = test_dtr_lst1.pop(1)
-test_dtr_str1 = test_dtr_lst1.pop(2)
-test_dtr_str1 = test_dtr_lst1.pop(2)
-test_dtr_str1 = test_dtr_lst1.pop(-1)
-test_dtr_str1 = test_dtr_lst1.pop(-1)
-print(test_dtr_lst1)
+test_dtr_lst1.pop(0)
+test_dtr_lst1.pop(1)
+test_dtr_lst1.pop(2)
+test_dtr_lst1.pop(2)
+test_dtr_lst1.pop(-1)
+test_dtr_lst1.pop(-1)
+st = test_dtr_lst2[8]+test_dtr_lst2[9]
 # print(test_dtr_lst[1])
 # print(test_dtr_lst[3])
 # print(test_dtr_lst[6])
@@ -23,7 +23,9 @@ print(test_dtr_lst1)
 # st = test_dtr_lst2[8]
 # st += test_dtr_lst2[9]
 
-st = test_dtr_lst2[8]+test_dtr_lst2[9]
 # st = st.join("")
 
-print(st)
+# print(st)
+
+test_dtr_lst1.append(st)
+print(test_dtr_lst1)
